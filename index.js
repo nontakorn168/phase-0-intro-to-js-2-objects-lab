97644232ba4bb2f924bfc9fe5678f65bef8e3031
nontakorn <nontakorn_chris@hotmail.com>
2024-02-25 12:27:37 -0500

    finish

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {};
+
+// Function to update employee with a new key-value pair (non-destructive)
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+// Function to update employee with a new key-value pair (destructive)
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Function to delete a key from a clone of employee (non-destructive)
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Function to delete a key from employee (destructive)
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
